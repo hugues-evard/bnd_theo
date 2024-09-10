0041963496ce9787d2338affeaf999031ec91e9e
Hugues EVARD <hugues.evard@cern.ch>
2024-09-10 11:42:34 +0200

    dat2root: Putting both pT for thigh and tlow in the same root file, plots_bnd: add hardcoded legend (WIP)

--- dat2root.py
+++ dat2root.py
@@ -2,48 +2,55 @@ import ROOT as rt
 from array import array
 import numpy as np
 
-# ======= I/O
+scales = ["HT2", "mttbar2"]
 
-indir = "./inputs/run_HT2/NLO-run/distributions__NLO_QCD/"
-outdir = "./outputs/"
-infile = "pT_thigh__NLO_QCD" 
+for scale in scales:
 
-# ======= reading file
+    # ======= I/O
 
-arr = np.genfromtxt(indir+infile+".dat", usecols=(0,1,2,3,4,5,6,7))
+    indir = f"./inputs/run_{scale}/NLO-run/distributions__NLO_QCD/"
+    outdir = "./outputs/"
 
-# number of bins
-nbins = len(arr)
+    outfile = rt.TFile.Open(outdir+scale+".root", "recreate")
 
-# binning
-bin_low = arr[:,0]
-bin_high = arr[:,1]
-bin_center = (bin_high + bin_low) / 2
+    # ======= reading files
 
-edges = array('d', list(bin_low) + [bin_high[-1]])
+    for infile in ["pT_thigh__NLO_QCD" , "pT_tlow__NLO_QCD"]:
 
-# scale value
-scale_center = arr[:, 2]
-scale_low = arr[:, 4]
-scale_high = arr[:, 6]
+        arr = np.genfromtxt(indir+infile+".dat", usecols=(0,1,2,3,4,5,6,7))
 
-# ======== converting to TGraphAsymmErrors
+        # number of bins
+        nbins = len(arr)
 
-graph = rt.TGraphAsymmErrors()
-graph.SetName(infile)
-graph.Set(nbins)
+        # binning
+        bin_low = arr[:,0]
+        bin_high = arr[:,1]
+        bin_center = (bin_high + bin_low) / 2
 
-# == filling the graph
-for i in range(nbins):
-    graph.SetPoint(i, bin_center[i], scale_center[i])
-    graph.SetPointError(i, bin_center[i] - bin_low[i], bin_high[i] - bin_center[i],
-            scale_center[i] - scale_low[i], scale_high[i] - scale_center[i],
-            )
+        edges = array('d', list(bin_low) + [bin_high[-1]])
 
-# ======== Saving file
+        # scale value
+        scale_center = arr[:, 2]
+        scale_low = arr[:, 4]
+        scale_high = arr[:, 6]
 
-outfile = rt.TFile.Open(outdir+infile+".root", "recreate")
-graph.Write()
-outfile.Close()
+        # ======== converting to TGraphAsymmErrors
+
+        graph = rt.TGraphAsymmErrors()
+        graph.SetName(infile)
+        graph.Set(nbins)
+
+        # == filling the graph
+        for i in range(nbins):
+            graph.SetPoint(i, bin_center[i], scale_center[i])
+            graph.SetPointError(i, bin_center[i] - bin_low[i], bin_high[i] - bin_center[i],
+                    scale_center[i] - scale_low[i], scale_high[i] - scale_center[i],
+                    )
+
+        # ======== Saving file
+
+        graph.Write()
+
+    outfile.Close()
 
 
